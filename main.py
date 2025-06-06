2ce025b51b8a8d79c635b69d71d59c1e4934fbff
samuelefonio <samuelefonio98@gmail.com>
2025-06-06 21:48:22 +0200

    added output_dim to the argparser

--- main.py
+++ main.py
@@ -118,6 +118,7 @@ def parse_args():
     parser.add_argument('-seed', dest='seed', default=None, type = int, help='ranking of the run')
     parser.add_argument('-t', dest='temperature', default=None, type = float, help='temperature')
     parser.add_argument('-ex', dest='ex_4_class', default=None, type = int, help='Number of examples per class if in config the key reduced is True')
+    parser.add_argument('-d', dest='output_dim', default=None, type = int, help='embedding dimension')
     parser.add_argument('-shrink', dest='shrink_init', action='store_true')
     args = parser.parse_args()
     return args
@@ -136,6 +137,9 @@ if __name__ == "__main__":
         config['temperature'] = args.temperature
     if args.ex_4_class is not None:
         config['dataset']['ex_4_class'] = args.ex_4_class
+    if args.output_dim is not None:
+        config['output_dim'] = args.output_dim
+
     config['shrink_init'] = args.shrink_init
     torch.manual_seed(config['seed'])
     torch.cuda.manual_seed_all(config['seed'])
