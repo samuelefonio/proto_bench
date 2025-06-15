a0b2bff76042929ea2f852153cb39ebc6d977d93
sgrosso <silvia.grosso@insa-lyon.fr>
2025-06-15 16:36:44 +0200

    configs for few data

--- run_experiments.sh
+++ run_experiments.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 
-nohup python3 main.py -config configs/config_ecl_cifar10_01_12345.yaml > log_ecl_cifar10_01_12345.out 2>&1
-nohup python3 main.py -config configs/config_ecl_aircraft_1_117.yaml > log_ecl_aircraft_1_117.out 2>&1
-nohup python3 main.py -config configs/config_ecl_aircraft_1_12345.yaml > log_ecl_aircraft_1_12345.out 2>&1
+nohup python3 main.py -config configs/config_hps_cub.yaml -seed 42 > log_hps_cub_42.out 2>&1
+nohup python3 main.py -config configs/config_hps_cub.yaml -seed 12345 > log_hps_cub_12345.out 2>&1
+nohup python3 main.py -config configs/config_hps_cub_sgd.yaml -seed 42 > log_hps_cub_sgd_42.out 2>&1
+nohup python3 main.py -config configs/config_hps_cub_sgd.yaml -seed 117 > log_hps_cub_sgd_117.out 2>&1
+nohup python3 main.py -config configs/config_hps_cub_sgd.yaml -seed 12345 > log_hps_cub_sgd_12345.out 2>&1
