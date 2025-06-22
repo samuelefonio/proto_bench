57f3f470ecd41ab1029778d11d54cf371e175c65
sgrosso <silvia.grosso@insa-lyon.fr>
2025-06-22 21:55:41 +0200

    hps configs for few data

--- run_experiments.sh
+++ run_experiments.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-nohup python3 main.py -config configs_fewdata/config_ecl_cifar10.yaml -seed 117 > log_ecl_cifar10_fewdata_5ex_def.out 2>&1 &
-nohup python3 main.py -config configs_fewdata/config_ecl_cifar10.yaml -seed 12345 > log_ecl_cifar10_fewdata_5ex_def.out 2>&1 &
+
+nohup python3 main.py -config configs_fewdata/config_hps_cifar10.yaml -lr 0.0003 -wd 0.0003 > log_hps_cifar10_fewdata_5.out 2>&1 &
+nohup python3 main.py -config configs_fewdata/config_hps_cifar10.yaml -lr 0.001 -wd 0.001 > log_hps_cifar10_fewdata_5.out 2>&1 &
+nohup python3 main.py -config configs_fewdata/config_hps_cifar10.yaml -lr 0.001 -wd 0.0003 > log_hps_cifar10_fewdata_5.out 2>&1 &
